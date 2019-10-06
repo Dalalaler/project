449f5ef737d247145315a08d4dd453a26ca2447f
repatik <dalalaler@gmail.com>
2019-10-07 01:53:51 +0300

    Added language choice

--- src/home/urls.py
+++ src/home/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
+    path("eng/", views.eng_index, name='eng_index'),
+    path("ru/", views.ru_index, name='index'),
+
 ]
--- src/home/views.py
+++ src/home/views.py
@@ -10,4 +10,20 @@ def index(request):
         ],
     })
 
+def eng_index(request):
+    return render(request, "eng.html", {
+        "data": [
+            "My Project",
+            system(),
+        ],
+    })
+
+def ru_index(request):
+    return render(request, "ru.html", {
+        "data": [
+            "My Project",
+            system(),
+        ],
+    })
+
 # Create your views here.
